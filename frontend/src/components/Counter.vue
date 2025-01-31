1ced869d66e1760df54748125451c30dd48848b0
magwoo <magwood51@yandex.ru>
2025-01-31 22:00:03 +0300

    added frontend

---
+++ frontend/src/components/Counter.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const counter = ref<number>(0);
+</script>
+
+<template>
+  <button
+    @click="counter++"
+    class="cursor-pointer rounded-md bg-blue-500 px-3 py-1 text-white"
+  >
+    click me! {{ counter }}
+  </button>
+</template>
