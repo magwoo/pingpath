48e6678b4d4c0a2f6a4e8a4c04f809245a40070a
magwoo <magwood51@yandex.ru>
2025-02-01 00:11:23 +0300

    created minimal api base

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello World!");
+use anyhow::Context;
+use axum::Router;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let router = Router::new();
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:80")
+        .await
+        .context("failed to bind addr")?;
+
+    axum::serve(listener, router)
+        .await
+        .context("failed to serve")?;
+
+    Ok(())
 }
