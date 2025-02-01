f2270be3c883d2cb6fd11721bd1f74835271d404
geeksaider <nikita.bondarev.06@gmail.com>
2025-02-01 15:34:45 +0300

    Create an pages base structure

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,13 @@
 import { createApp } from "vue";
 import { createRouter, createWebHistory, RouterView } from "vue-router";
 import Index from "./pages/index.vue";
+import Profile from "./pages/profile.vue"
 
 import "./main.css";
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: "/", component: Index }],
+  routes: [{ path: "/", component: Index }, { path: "/profile", component: Profile }],
 });
 
 createApp(RouterView).use(router).mount("body");
