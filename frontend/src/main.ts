4a120f29546b86b06114e85472d0b2ad62d280b4
magwoo <magwood51@yandex.ru>
2025-02-01 16:26:51 +0300

    added new npm script and first run then

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,15 +1,15 @@
 import { createApp } from "vue";
 import { createRouter, createWebHistory, RouterView } from "vue-router";
 import Index from "./pages/index.vue";
-import Profile from "./pages/profile.vue"
+import Profile from "./pages/profile.vue";
 
 import "./main.css";
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [ 
-    { path: "/", component: Index }, 
-    { path: "/profile", component: Profile }
+  routes: [
+    { path: "/", component: Index },
+    { path: "/profile", component: Profile },
   ],
 });
 
--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -4,13 +4,13 @@ import Counter from "@/components/Counter.vue";
 
 <template>
   <main
-    class="flex flex-col justify-center items-center gap-8 h-screen pb-[20vh]"
+    class="flex h-screen flex-col items-center justify-center gap-8 pb-[20vh]"
   >
     <div class="flex items-start gap-3">
       <h1 class="text-4xl text-stone-600">
         Vue + Vite + Tailwind + Typescript
       </h1>
-      <span class="text-stone-500 text-lg">template</span>
+      <span class="text-lg text-stone-500">template</span>
     </div>
     <Counter />
   </main>
