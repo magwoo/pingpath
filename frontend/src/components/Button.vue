324ac5e2ccb430e603fb8bf8dbad152e61a27b73
Igor Zhorkin <zhorkinigor@yandex.ru>
2025-02-02 12:43:23 +0300

    refactoring

--- frontend/src/components/Button.vue
+++ frontend/src/components/Button.vue
@@ -1,31 +1,27 @@
 <script setup lang="ts">
-const BUTTON_VARIANTS = {
-    "accent": "bg-accent text-white cursor-pointer",
-    "neutral": "bg-neutral/10 cursor-pointer text-neutral/75",
-    "transparent": "bg-transparent text-neutral/75"
-}
-
-const BUTTON_SIZES = {
-    "small": "px-3 py-1",
-    "medium": "px-8 py-2",
-    "large": "px-8 py-2 text-xl",
-}
-
-type ButtonVariant = keyof typeof BUTTON_VARIANTS;
-type ButtonSize = keyof typeof BUTTON_SIZES;
-
 interface Props {
-    readonly variant?: ButtonVariant,
-    readonly size?: ButtonSize,
+    readonly variant?: 'accent' | 'neutral' | 'transparent',
+    readonly size?: 'sm' | 'md' | 'lg',
     readonly icon?: string,
 }
 
-const { variant = 'accent', size = 'small', icon } = defineProps<Props>();
+const { variant = 'accent', size = 'sm', icon } = defineProps<Props>();
 
+const buttonClass = {
+    "accent": "bg-accent text-white cursor-pointer",
+    "neutral": "bg-neutral/10 cursor-pointer text-neutral/75",
+    "transparent": "bg-transparent text-neutral/75"
+}[variant];
+
+const buttonSize = {
+    "sm": "px-3 py-1",
+    "md": "px-8 py-2",
+    "lg": "px-8 py-2 text-xl",
+}[size];
 </script>
 
 <template>
-    <button class="flex gap-2 items-center" :class="[BUTTON_VARIANTS[variant], BUTTON_SIZES[size]]">
+    <button class="flex gap-2 items-center" :class="[buttonClass, buttonSize]">
         <img v-if="icon" class="size-5 shrink-0" :src="icon" >
         <slot></slot>
     </button>
