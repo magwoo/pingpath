bbc20ed26d3a259ff8027745f9abdde2821e9abe
Igor Zhorkin <zhorkinigor@yandex.ru>
2025-02-02 14:07:18 +0300

    create link component

---
+++ frontend/src/components/Link.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const LINK_VARIANTS = {
+    "accent": "text-accent text-xl",
+    "neutral": "text-neutral/75 text-xl"
+}
+
+interface Props {
+    variant: keyof typeof LINK_VARIANTS;
+}
+
+const {variant} = defineProps<Props>();
+
+</script>
+
+<template>
+    <RouterLink to="/" class="flex gap-2" :class="LINK_VARIANTS[variant]">
+        <slot name="before"></slot>
+        <slot></slot>
+        <slot name="after"></slot>
+    </RouterLink>
+</template>
