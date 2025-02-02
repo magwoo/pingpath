8a21daed7ad2ffaafde768dfdfd4d88c1f9979ec
geeksaider <nikita.bondarev.06@gmail.com>
2025-02-02 18:51:27 +0300

    add Github logo, add flex for nav

--- frontend/src/components/NavPanel.vue
+++ frontend/src/components/NavPanel.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Link from "./Link.vue";
 import Logo from "./svg/Logo.vue";
+import Button from "./Button.vue";
+import GitHub from "./svg/GitHub.vue";
 </script>
 <template>
-  <section class="my-12">
+  <section class="my-12 flex justify-between">
     <nav class="flex gap-10">
       <Link class="group">
         <template #before="isActive" v-slot="isActive">
