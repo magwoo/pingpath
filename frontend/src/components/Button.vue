2ce7e1154ca685ab6f9312fe6685ea9cf552f8f1
Igor Zhorkin <zhorkinigor@yandex.ru>
2025-02-02 13:02:09 +0300

    added slots before & after

--- frontend/src/components/Button.vue
+++ frontend/src/components/Button.vue
@@ -1,28 +1,31 @@
 <script setup lang="ts">
-interface Props {
-    readonly variant?: 'accent' | 'neutral' | 'transparent',
-    readonly size?: 'sm' | 'md' | 'lg',
-    readonly icon?: string,
-}
-
-const { variant = 'accent', size = 'sm', icon } = defineProps<Props>();
-
-const buttonClass = {
+const BUTTON_VARIANTS = {
     "accent": "bg-accent text-white cursor-pointer",
     "neutral": "bg-neutral/10 cursor-pointer text-neutral/75",
     "transparent": "bg-transparent text-neutral/75"
-}[variant];
+}
 
-const buttonSize = {
+const BUTTON_SIZES = {
     "sm": "px-3 py-1",
     "md": "px-8 py-2",
     "lg": "px-8 py-2 text-xl",
-}[size];
+}
+
+type ButtonVariant = keyof typeof BUTTON_VARIANTS;
+type ButtonSize = keyof typeof BUTTON_SIZES;
+
+interface Props {
+    readonly variant?: ButtonVariant,
+    readonly size?: ButtonSize,
+}
+
+const { variant = 'accent', size = 'sm', } = defineProps<Props>();
 </script>
 
 <template>
-    <button class="flex gap-2 items-center" :class="[buttonClass, buttonSize]">
-        <img v-if="icon" class="size-5 shrink-0" :src="icon" >
+    <button class="flex gap-2 items-center" :class="[BUTTON_VARIANTS[variant], BUTTON_SIZES[size]]">
+        <slot name="before"></slot>
         <slot></slot>
+        <slot name="after"></slot>
     </button>
 </template>
