86ffdb9e20304435053909463194aa6e4d1e4654
geeksaider <132765681+geeksaider@users.noreply.github.com>
2025-02-03 12:12:48 +0000

    Create profile photo from api

--- frontend/src/pages/profile.vue
+++ frontend/src/pages/profile.vue
@@ -3,12 +3,26 @@ import Nav from "@/components/Nav.vue";
 import HStack from "@/components/HStack.vue";
 import VStack from "@/components/VStack.vue";
 import Text from "@/components/Text.vue";
+import { onMounted, ref } from "vue";
+
+const data = ref();
+
+onMounted(async () => {
+  await fetch("/api/auth/dev", { method: "POST" });
+  const response = await fetch("/api/profile");
+
+  data.value = JSON.parse(await response.json());
+  console.log(data.value);
+});
 </script>
 
 <template>
   <Nav></Nav>
   <VStack class="gap-8">
     <Text size="lg" variant="neutral">Профиль</Text>
-    <HStack> </HStack>
+    <HStack>
+      <img v-if="data" :src="data.iconUrl" class="size-64" alt="" />
+      <div v-else class="bg-neutral size-64"></div>
+    </HStack>
   </VStack>
 </template>
