965a571dbd7ee50e839ce51218837b61f6e455cf
magwoo <magwood51@yandex.ru>
2025-02-03 21:51:12 +0300

    add popup arrow z-index

--- frontend/src/components/map/MessagePopup.vue
+++ frontend/src/components/map/MessagePopup.vue
@@ -11,7 +11,7 @@ import VStack from "../VStack.vue";
     <Text class="text-white" size="md"> Нидерланды, <br />Амстердам </Text>
     <span class="text-success text-xs">задержка: 12мс</span>
     <div
-      class="bg-accent absolute right-0 -bottom-2 left-0 mx-auto size-5 rotate-45"
+      class="bg-accent absolute right-0 -bottom-2 left-0 mx-auto size-5 rotate-45 -z-10"
     />
   </VStack>
 </template>
