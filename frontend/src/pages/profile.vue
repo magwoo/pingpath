5baada9b32e9bfb64699261a7dc1b89e5bf5a6c7
geeksaider <132765681+geeksaider@users.noreply.github.com>
2025-02-03 09:58:14 +0000

    Create about profile skelet

--- frontend/src/pages/profile.vue
+++ frontend/src/pages/profile.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
 import Nav from "@/components/Nav.vue";
+import HStack from "@/components/HStack.vue";
+import VStack from "@/components/VStack.vue";
+import Text from "@/components/Text.vue";
 </script>
 
 <template>
   <Nav></Nav>
+  <VStack class="gap-8">
+    <Text size="lg" variant="neutral">Профиль</Text>
+    <HStack> </HStack>
+  </VStack>
 </template>
