718f7592a03c9819c776aebbfc2a41e4379f87e0
magwoo <magwood51@yandex.ru>
2025-02-07 21:03:10 +0300

    added locations table

--- migrations/0_initial.sql
+++ migrations/0_initial.sql
@@ -4,3 +4,13 @@ CREATE TABLE IF NOT EXISTS `users` (
     `github_token` TEXT,
     `icon_url` TEXT
 );
+
+CREATE TABLE IF NOT EXISTS `locations` (
+    `id` INTEGER PRIMARY KEY,
+    `addr` TEXT,
+    `country` TEXT NOT NULL CHECK(length(country) == 2),
+    `city` TEXT,
+    `latitude` INTEGER NOT NULL,
+    `longitude` INTEGER NOT NULL
+);
+
