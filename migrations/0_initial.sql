e52675519edbb42b74e2d2cbbc49c3a5d49da79b
magwoo <magwood51@yandex.ru>
2025-02-07 21:54:09 +0300

    added user addrs and pings tables

--- migrations/0_initial.sql
+++ migrations/0_initial.sql
@@ -2,7 +2,8 @@ CREATE TABLE IF NOT EXISTS `users` (
     `id` INTEGER PRIMARY KEY,
     `username` TEXT NOT NULL DEFAULT('guest'),
     `github_token` TEXT,
-    `icon_url` TEXT
+    `icon_url` TEXT,
+    `create_date` DATETIME NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `locations` (
@@ -12,12 +13,21 @@ CREATE TABLE IF NOT EXISTS `locations` (
     `city` TEXT,
     `latitude` INTEGER NOT NULL,
     `longitude` INTEGER NOT NULL
+    `create_date` DATETIME NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `user_addrs` (
     `id` INTEGER PRIMARY KEY,
     `name` TEXT,
     `addr` TEXT NOT NULL,
+    `user_id` INTEGER NOT NULL,
     `create_date` DATETIME NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS `pings` (
+    `id` INTEGER PRIMARY KEY,
+    `addr_id` INTEGER NOT NULL,
+    `latency` TEXT NOT NULL,
+    `start_date` DATETIME NOT NULL
+)
 
