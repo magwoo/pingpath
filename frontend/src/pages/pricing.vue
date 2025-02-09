e815708b7accf13eb89b65f1dee1733cd3883e59
Igor Zhorkin <zhorkinigor@yandex.ru>
2025-02-09 14:30:11 +0300

    small changes

--- frontend/src/pages/pricing.vue
+++ frontend/src/pages/pricing.vue
@@ -7,8 +7,8 @@ import VStack from "@/components/VStack.vue";
 
 <template>
   <Nav />
-  <VStack class="mt-24 min-h-[101vh] gap-8">
-    <Text variant="neutral" size="lg" class="font-bold opacity-50">
+  <VStack class="mt-24 gap-8">
+    <Text variant="neutral" size="lg" class="font-semibold opacity-50">
       Наши тарифы
     </Text>
     <HStack class="gap-16">
@@ -27,15 +27,15 @@ import VStack from "@/components/VStack.vue";
         </Text>
       </VStack>
       <VStack class="w-64 gap-12">
-        <Text variant="neutral" size="lg" class="opacity-75"> Пробный </Text>
-        <Text variant="neutral" size="md" class="text-warn"> 50 </Text>
-        <Text variant="neutral" size="md" class="text-error"> Нет </Text>
-        <Text variant="neutral" size="md" class="text-error"> Нет </Text>
+        <Text variant="neutral" size="lg" class="opacity-75">Пробный</Text>
+        <Text variant="neutral" size="md" class="text-warn">50</Text>
+        <Text variant="neutral" size="md" class="text-error">Нет</Text>
+        <Text variant="neutral" size="md" class="text-error">Нет</Text>
       </VStack>
       <VStack class="w-64 gap-12">
-        <Text variant="neutral" size="lg" class="opacity-75"> Полный </Text>
-        <Text variant="neutral" size="md" class="text-success"> 500 </Text>
-        <Text variant="neutral" size="md" class="text-success"> Да </Text>
+        <Text variant="neutral" size="lg" class="opacity-75">Полный</Text>
+        <Text variant="neutral" size="md" class="text-success">500</Text>
+        <Text variant="neutral" size="md" class="text-success">Да</Text>
         <Text variant="neutral" size="md" class="text-success">csv, json</Text>
       </VStack>
     </HStack>
