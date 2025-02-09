96c196491ff1716bcf4972453c8d2b000b65c116
Igor Zhorkin <zhorkinigor@yandex.ru>
2025-02-09 14:39:12 +0300

    add const notifcation container id

--- frontend/src/composables/useNotifications.ts
+++ frontend/src/composables/useNotifications.ts
@@ -2,14 +2,16 @@ import Notification from "@/components/notifications/Notification.vue";
 
 import { createApp, h, Transition } from "vue";
 
+const NOTIFICATION_CONTAINER_ID = "notifications-container";
+
 const createContainer = () => {
-  const container = document.getElementById("notifications-container");
+  const container = document.getElementById(NOTIFICATION_CONTAINER_ID);
 
   if (container) return container;
 
   const notificationsContainer = document.createElement("div");
 
-  notificationsContainer.id = "notifications-container";
+  notificationsContainer.id = NOTIFICATION_CONTAINER_ID;
   notificationsContainer.className =
     "fixed right-6 bottom-6 w-[320px] flex flex-col gap-6 items-end pointer-events-none";
 
