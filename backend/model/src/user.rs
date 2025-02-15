5ed6956ca1ea5c79a4f823abdec4e0b3ed0bdce2
magwoo <magwood51@yandex.ru>
2025-02-15 16:14:05 +0300

    added user from row deser

--- backend/model/src/user.rs
+++ backend/model/src/user.rs
@@ -1,12 +1,43 @@
-use chrono::NaiveDateTime;
+use anyhow::Context;
+use rqlite_rs::{query, FromRow};
 
 use self::github::UserGithub;
+use crate::prelude::*;
 
 pub mod github;
 
 pub struct User {
     id: i64,
     username: String,
-    created_at: NaiveDateTime,
-    github: UserGithub,
+    created_at: Datetime,
+    github: Option<UserGithub>,
+}
+
+impl User {
+    pub async fn create_guest(db: Database) -> anyhow::Result<Self> {
+        let now = Datetime::now();
+
+        let query = query!(
+            "INSERT INTO users(username, created_at) VALUES (?, ?)",
+            "guest",
+            now
+        )
+        .context("failed to parse query")?;
+
+        let id = db.exec(query).await?.last_insert_id();
+    }
+}
+
+impl FromRow for User {
+    fn from_row(row: rqlite_rs::Row) -> Result<Self, rqlite_rs::IntoTypedError> {
+        Ok(Self {
+            id: row.get("id")?,
+            username: row.get("username")?,
+            created_at: row.get("created_at")?,
+            github: match row.get_opt::<String>("token")?.is_some() {
+                true => Some(UserGithub::from_row(row)?),
+                false => None,
+            },
+        })
+    }
 }
--- backend/model/src/user/github.rs
+++ backend/model/src/user/github.rs
@@ -1,4 +1,15 @@
+use rqlite_rs::FromRow;
+
 pub struct UserGithub {
     token: String,
     img_url: String,
+}
+
+impl FromRow for UserGithub {
+    fn from_row(row: rqlite_rs::Row) -> Result<Self, rqlite_rs::IntoTypedError> {
+        Ok(Self {
+            token: row.get("token")?,
+            img_url: row.get("img_url")?,
+        })
+    }
 }
