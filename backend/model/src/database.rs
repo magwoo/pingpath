4407589ca3314090de05966ce51f8c6b60060b30
magwoo <magwood51@yandex.ru>
2025-02-17 14:25:01 +0300

    update database model

--- backend/model/src/database.rs
+++ backend/model/src/database.rs
@@ -3,11 +3,25 @@ use rqlite_rs::{prelude::*, query::RqliteQuery};
 
 static mut INSTANCE: Option<RqliteClient> = None;
 
-#[derive(Clone, Copy)]
-pub struct Database;
+#[derive(Clone, Copy, Default)]
+pub struct Rqlite;
 
-impl Database {
-    pub fn from_env() -> Result<Self> {
+pub trait Database: Sized + Default {
+    fn from_env() -> Result<Self>;
+
+    fn inner<'a>() -> &'a RqliteClient;
+
+    async fn exec(q: RqliteQuery) -> Result<QueryResult>;
+
+    async fn fetch<T: FromRow>(q: RqliteQuery) -> Result<Vec<T>>;
+
+    async fn fetch_optional<T: FromRow>(q: RqliteQuery) -> Result<Option<T>>;
+
+    async fn fetch_one<T: FromRow>(q: RqliteQuery) -> Result<T>;
+}
+
+impl Database for Rqlite {
+    fn from_env() -> Result<Self> {
         let host = std::env::var("DATABASE_URL").context("missing database url env var")?;
 
         let inner = RqliteClientBuilder::new()
@@ -21,11 +35,11 @@ impl Database {
     }
 
     #[inline]
-    pub fn inner<'a>() -> &'a RqliteClient {
+    fn inner<'a>() -> &'a RqliteClient {
         inner()
     }
 
-    pub async fn exec(q: RqliteQuery) -> Result<QueryResult> {
+    async fn exec(q: RqliteQuery) -> Result<QueryResult> {
         let query = q.query.clone();
 
         Self::inner()
@@ -34,16 +48,33 @@ impl Database {
             .with_context(|| format!("failed to exec query({query})"))
     }
 
-    pub async fn fetch(&self, q: RqliteQuery) -> Result<Vec<Row>> {
+    async fn fetch<T: FromRow>(q: RqliteQuery) -> Result<Vec<T>> {
         let query = q.query.clone();
 
         Self::inner()
             .fetch(q)
             .await
-            .with_context(|| format!("failed to fetch query({query})"))
+            .with_context(|| format!("failed to fetch query({query})"))?
+            .into_typed()
+            .context("failed to type")
     }
 
-    pub async fn fetch_optional(&self, q: RqliteQuery) -> Result<Option<Row>> {
+    async fn fetch_optional<T: FromRow>(q: RqliteQuery) -> Result<Option<T>> {
+        let query = q.query.clone();
+
+        let row = Self::inner()
+            .fetch(q)
+            .await
+            .with_context(|| format!("failed to fetch query({query})"))?
+            .into_typed::<T>()
+            .context("failed to type")?
+            .into_iter()
+            .next();
+
+        Ok(row)
+    }
+
+    async fn fetch_one<T: FromRow>(q: RqliteQuery) -> Result<T> {
         let query = q.query.clone();
 
         let row = Self::inner()
@@ -51,21 +82,10 @@ impl Database {
             .await
             .with_context(|| format!("failed to fetch query({query})"))?
             .into_iter()
-            .next();
-
-        Ok(row)
-    }
-
-    pub async fn fetch_one(&self, q: RqliteQuery) -> Result<Row> {
-        let query = q.query.clone();
-
-        Self::inner()
-            .fetch(q)
-            .await
-            .with_context(|| format!("failed to fetch query({query})"))?
-            .into_iter()
             .next()
-            .with_context(|| format!("missing fetch_one row on query({query})"))
+            .with_context(|| format!("missing fetch_one row on query({query})"))?;
+
+        row.into_typed().context("failed to type")
     }
 }
 
